d9fb6249df6adae0bb6b912a02c2293619b03ebd
djnicktest <cardenasvargasnicolas@gmail.com>
2024-07-29 20:56:33 -0500

    Update djnicktest.cs

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/djnicktest.cs
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/djnicktest.cs
@@ -1,8 +1,32 @@
+// https://dotnet.microsoft.com/es-es/languages/csharp//
+
 // Asi se hace un comentario en una linea //
 
 /* asi se realiza un comentario
 en dos lineas 
 o mas lineas*/
 
+string Nombre = Nicolas;
+const int Mes = 12;
+
+int Edad = 34;
+short Carros = 3;
+long precio = 2555830;
+bool esFalso = false;
+
+byte memoria = 150;
+sbyte memoria2 = -123;
+
+float Valor= 2.5f;
+double valorDouble = 120.45;
+
+decimal valorDecimal = 12.4;
+
+string apellido = "Cardenas";
+
+char caracter = @;
+
+Console.Write ("Hola!, el lenguaje que estoy usando es C#");
+Console.WriteLine ("Hola!, el lenguaje que uso es C#");
 
 

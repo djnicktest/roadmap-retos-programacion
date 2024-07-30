76889889945c3242eddfb8c8bc95cee393620fbc
djnicktest <cardenasvargasnicolas@gmail.com>
2024-07-30 14:15:15 -0500

    update djnicktest.cs

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/c#/djnicktest.cs
@@ -0,0 +1,28 @@
+// Operadores Aritmeticos //
+
+int a=3,b=10;
+int suma = a + b; // suma
+
+int c=15, d= 5;
+int Resta= c-d; // Resta
+
+int e= 8, f= 30;
+int Multi= e*f; // Multiplicacion
+
+int g= 250, h= 6;
+int div = g/h; // Division
+
+int i= 12, j= 4;
+int mod= i % j; // Modulo o resto
+
+// Operadores de Asignacion //
+
+int y = 500; // Asignacion de valor a una variable 
+
+int aa = 20;
+int aa += 12;// agrega el valor de la derecha a la variable de la izquierda y asigna el resultado a esa variabe aa = 32 
+
+int bb = 30;
+int bb -= 15; // resta el valor de la derecha a la variable de la izquierda y asigna el resultado a esa variable bb = 15 
+
+
